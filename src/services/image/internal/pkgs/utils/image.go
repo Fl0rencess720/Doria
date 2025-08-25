d22d0bf65c8aed13c9a57d248c9f1ef176921ffc
Fl0rencess720 <2391793239@qq.com>
2025-08-25 21:48:44 +0800

    fix(image): 支持上传多种格式的图片

--- src/services/image/internal/pkgs/utils/image.go
+++ src/services/image/internal/pkgs/utils/image.go
@@ -3,10 +3,12 @@ package utils
 import (
 	"encoding/base64"
 	"fmt"
+	"net/http"
 )
 
 func GenImageDataURI(data []byte) string {
+	mimeType := http.DetectContentType(data)
 	base64Data := base64.StdEncoding.EncodeToString(data)
-	dataURI := fmt.Sprintf("data:image/jpeg;base64,%s", base64Data)
+	dataURI := fmt.Sprintf("data:%s;base64,%s", mimeType, base64Data)
 	return dataURI
 }
