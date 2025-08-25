49beee01734e31c0f20976c6b8a145d8796aae65
Fl0rencess720 <2391793239@qq.com>
2025-08-25 23:12:34 +0800

    feat(gateway): ChatStream SSE 接口

---
+++ src/gateway/service/chat/route.go
@@ -0,0 +1,10 @@
+package chat
+
+import (
+	"github.com/Fl0rencess720/Bonfire-Lit/src/gateway/internal/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func InitApi(group *gin.RouterGroup, cu *controllers.ChatUseCase) {
+	group.POST("/send")
+}
