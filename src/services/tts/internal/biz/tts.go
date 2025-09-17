6ed5f1938cd541db2d9ccf6a50fc0981db774c1d
Fl0rencess720 <2391793239@qq.com>
2025-09-17 23:42:24 +0800

    feat(tts): 支持讯飞语音合成

--- src/services/tts/internal/biz/tts.go
+++ src/services/tts/internal/biz/tts.go
@@ -1,5 +1,14 @@
 package biz
 
+import (
+	"encoding/base64"
+	"encoding/json"
+
+	"github.com/Fl0rencess720/Bonfire-Lit/src/services/tts/pkgs/utils"
+	"github.com/gorilla/websocket"
+	"github.com/spf13/viper"
+)
+
 type TTSUseCase struct {
 	repo TTSRepo
 }
@@ -7,6 +16,111 @@ type TTSUseCase struct {
 type TTSRepo interface {
 }
 
+type TTSRequest struct {
+	Common   Common   `json:"common"`
+	Business Business `json:"business"`
+	Data     Data     `json:"data"`
+}
+
+type Common struct {
+	AppID string `json:"app_id"`
+}
+
+type Business struct {
+	Aue    string `json:"aue"`    // 音频格式
+	Sfl    int    `json:"sfl"`    // 流式返回MP3标志
+	Vcn    string `json:"vcn"`    // 发音人
+	Speed  int    `json:"speed"`  // 语速 [0-100]
+	Volume int    `json:"volume"` // 音量 [0-100]
+	Pitch  int    `json:"pitch"`  // 音高 [0-100]
+	Tte    string `json:"tte"`    // 文本编码格式
+}
+
+type Data struct {
+	Text   string `json:"text"`   // base64编码的文本
+	Status int    `json:"status"` // 固定为2
+}
+
 func NewTTSUseCase(repo TTSRepo) *TTSUseCase {
 	return &TTSUseCase{repo: repo}
+}
+
+func (u *TTSUseCase) SynthesizeSpeech(text string) ([]byte, error) {
+	authUrl := utils.AssembleAuthUrl(viper.GetString("tts.host_url"), viper.GetString("XF_API_KEY"), viper.GetString("XF_API_SECRET"))
+
+	conn, _, err := websocket.DefaultDialer.Dial(authUrl, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	textBase64 := base64.StdEncoding.EncodeToString([]byte(text))
+
+	req := TTSRequest{
+		Common: Common{AppID: viper.GetString("XF_APP_ID")},
+		Business: Business{
+			Aue:    "lame",
+			Sfl:    1,
+			Vcn:    "xiaoyan",
+			Speed:  50,
+			Volume: 50,
+			Pitch:  50,
+			Tte:    "UTF8",
+		},
+		Data: Data{
+			Text:   textBase64,
+			Status: 2,
+		},
+	}
+
+	reqBytes, err := json.Marshal(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := conn.WriteMessage(websocket.TextMessage, reqBytes); err != nil {
+		return nil, err
+	}
+
+	var audioData []byte
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			return nil, err
+		}
+
+		var resp struct {
+			Code    int    `json:"code"`
+			Message string `json:"message"`
+			Sid     string `json:"sid"`
+			Data    struct {
+				Audio  string `json:"audio"`
+				Status int    `json:"status"`
+				Ced    string `json:"ced"`
+			} `json:"data"`
+		}
+
+		if err := json.Unmarshal(message, &resp); err != nil {
+			return nil, err
+		}
+
+		if resp.Code != 0 {
+			break
+		}
+
+		if resp.Data.Audio != "" {
+			audio, err := base64.StdEncoding.DecodeString(resp.Data.Audio)
+			if err != nil {
+				return nil, err
+			} else {
+				audioData = append(audioData, audio...)
+			}
+		}
+
+		if resp.Data.Status == 2 {
+			break
+		}
+	}
+
+	return audioData, nil
 }
