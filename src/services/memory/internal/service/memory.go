3aa90c3abd316c5d060b0ce1809352204b2bbf9d
Fl0rencess720 <2391793239@qq.com>
2025-09-25 11:32:39 +0800

    feat(memory): 添加GetMemory服务方法

--- src/services/memory/internal/service/memory.go
+++ src/services/memory/internal/service/memory.go
@@ -1,1 +1,48 @@
 package service
+
+import (
+	"context"
+
+	memoryapi "github.com/Fl0rencess720/Doria/src/rpc/memory"
+	"github.com/Fl0rencess720/Doria/src/services/memory/internal/biz"
+)
+
+func (s *MemoryService) GetMemory(ctx context.Context, req *memoryapi.GetMemoryRequest) (*memoryapi.GetMemoryResponse, error) {
+	memory, err := s.memoryUseCase.RetrieveMemory(ctx, uint(req.UserId), req.Prompt)
+	if err != nil {
+		return nil, err
+	}
+
+	stm := make([]*memoryapi.ShortMidTermMemory, len(memory))
+	mtm := make([]*memoryapi.ShortMidTermMemory, len(memory))
+	ltm := make([]*memoryapi.LongTermMemory, len(memory))
+
+	for _, m := range memory {
+		switch m.Status {
+		case biz.QAStatusInSTM:
+			stm = append(stm, &memoryapi.ShortMidTermMemory{
+				UserInput:   m.UserInput,
+				AgentOutput: m.AgentOutput,
+			})
+		case biz.QAStatusInMTM:
+			mtm = append(mtm, &memoryapi.ShortMidTermMemory{
+				UserInput:   m.UserInput,
+				AgentOutput: m.AgentOutput,
+			})
+		case biz.QAStatusInLTM:
+			ltm = append(ltm, &memoryapi.LongTermMemory{
+				Context: m.UserInput + m.AgentOutput,
+			})
+		default:
+			continue
+		}
+	}
+
+	resp := &memoryapi.GetMemoryResponse{
+		ShortTermMemory: stm,
+		MidTermMemory:   mtm,
+		LongTermMemory:  ltm,
+	}
+
+	return resp, nil
+}
